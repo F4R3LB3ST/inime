ee2a4f186666af5f489b37b476d5bc0f5cba625e
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-18 20:07:00 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <!DOCTYPE HTML>
 <html>
+  <style>
+		body {
+		background-image: linear-gradient(#2BD449, #56DC6D);
+		margin:0;
+		height:100%;}
+		.topbar {
+		float: left;
+		width: 100%;
+		height:8%
+		}
+	</style>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <head>
-    <title>Nonton Anime Sub Indo Gratis</title>
+    <title>Test Website</title>
   </head>
   <body>
     <?php
