df8279dc060b3d2f0da2482d6c8dda1f71914cba
F4R3LB3ST <superfarel2ymail.com@gmail.com>
2020-08-18 20:39:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 	body {
 		background-image: linear-gradient(#2BD449, #56DC6D);
 		margin:0;
-		height:1366px;
-		width:768px;
+		height:1920px;
+		width:1080px;
+	  }
+	  .topbar {
+		float: left;
+		width: 100%;
+		height:7.68%;
+		background-image:1d8f32
 	  }
 	</style>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +19,9 @@
     <title>Test Website</title>
   </head>
   <body>
+	  <div>
+		  
+	  </div>
     <?php
     echo "hi";
     ?>
